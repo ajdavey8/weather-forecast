dc8c8e990ddd1d1dcab1e1069774c2e71776d6f4
Andrew Davey <ajdavey8@gmail.com>
2018-04-24 22:31:02 +0100

    Add averages for all charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?&units=metric&appid=${API_KEY}`;
 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
